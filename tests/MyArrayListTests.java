2814f311afb91a5fab57387b0f95db7cb68dd07f
Joseph Abad <jabadjka3@gmail.com>
2015-10-08 16:20:26 -0400

    updated all test cases

--- tests/MyArrayListTests.java
+++ tests/MyArrayListTests.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
+
 import root.MyArrayList;
 import tests.Node;
 
@@ -17,6 +19,7 @@ public class MyArrayListTests {
 	static Node node2;
 	static Node node3;
 	static Node node4;
+	static Node node5;
 	static Method getCapacity;
 	static Method getArray;
 	static Method resize;
@@ -29,6 +32,7 @@ public class MyArrayListTests {
 		node2 = new Node(2, "Jose");
 		node3 = new Node(3, "Vicki");
 		node4 = new Node(4, "Luis");
+		node5 = new Node(5, "Ariel");
 		
 		//Initialize Private Methods
 		try {getCapacity =	MyArrayList.class.getDeclaredMethod("getCapacity");
@@ -49,7 +53,16 @@ public class MyArrayListTests {
 	@Test
 	public void testInitial1() {
 		//Caes: arraylist initialization
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));
+	}
+	@Test
+	public void testInitial2() {
+		//Caes: arraylist size initialization
+		arraylist = new MyArrayList(20);
+		assertTrue(testParameters(20, 0, whatShouldArrayListContainNow));
+		System.out.println("s");
+		resize();
+		assertTrue(testParameters(40, 0, whatShouldArrayListContainNow));
 	}
 	@Test
 	public void testAdd1() {
@@ -57,42 +70,44 @@ public class MyArrayListTests {
 		Object x = null;
 		arraylist.add(null);
 		arraylist.add(x);
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));
+		whatShouldArrayListContainNow.add(null);
+		whatShouldArrayListContainNow.add(null);
+		assertTrue(testParameters(10, 2, whatShouldArrayListContainNow));
 	}
 	@Test
 	public void testAdd2() {
 		//Case: simple add
 		arraylist.add(node1);
 		whatShouldArrayListContainNow.add(node1);
-		assertTrue(testParameters(11, 1, whatShouldArrayListContainNow));
+		assertTrue(testParameters(10, 1, whatShouldArrayListContainNow));
 	}
 	@Test
 	public void testAdd3() {
 		//Case: duplicate entries
-		arraylist.add(node1.getId(), node1);
-		arraylist.add(node1.getId(), node1);		
-		arraylist.add(node1.getId(), node1);
-		arraylist.add(node1.getId(), node1);
+		arraylist.add(node1);
+		arraylist.add(node1);		
+		arraylist.add(node1);
+		arraylist.add(node1);
 		whatShouldArrayListContainNow.add(node1);
 		whatShouldArrayListContainNow.add(node1);
 		whatShouldArrayListContainNow.add(node1);
 		whatShouldArrayListContainNow.add(node1);
-		assertTrue(testParameters(11, 4, whatShouldArrayListContainNow));
+		assertTrue(testParameters(10, 4, whatShouldArrayListContainNow));
 	}
 	@Test
 	public void testAdd4() {
 		//Case: many entries, one duplicate
-		arraylist.add(node1.getId(), node1);
-		arraylist.add(node1.getId(), node1);		
-		arraylist.add(node2.getId(), node2);
-		arraylist.add(node3.getId(), node3);
-		arraylist.add(node4.getId(), node4);		
+		arraylist.add(node1);
+		arraylist.add(node1);		
+		arraylist.add(node2);
+		arraylist.add(node3);
+		arraylist.add(node4);		
 		whatShouldArrayListContainNow.add(node1);
 		whatShouldArrayListContainNow.add(node1);
 		whatShouldArrayListContainNow.add(node2);
 		whatShouldArrayListContainNow.add(node3);
 		whatShouldArrayListContainNow.add(node4);
-		assertTrue(testParameters(11, 5, whatShouldArrayListContainNow));
+		assertTrue(testParameters(10, 5, whatShouldArrayListContainNow));
 	}
 	@Test
 	public void testAdd5() {
@@ -103,12 +118,35 @@ public class MyArrayListTests {
 		whatShouldArrayListContainNow.add(node3);
 		whatShouldArrayListContainNow.add(node2);
 		whatShouldArrayListContainNow.add(node1);
-		assertTrue(testParameters(11, 3, whatShouldArrayListContainNow));
+		assertTrue(testParameters(10, 3, whatShouldArrayListContainNow));
 	}
 	@Test
 	public void testAdd6() {
 		//Case: Keep testing inserting over indexes. Test the edges. Test the resizes.
-		assertTrue(false);
+		arraylist.add(0, node1);
+		arraylist.add(0, node2);
+		arraylist.add(0, node3);
+		arraylist.add(0, node4);
+		arraylist.add(0, node5);
+		arraylist.add(0, node1);
+		arraylist.add(0, node2);
+		arraylist.add(0, node3);
+		arraylist.add(0, node4);
+		arraylist.add(0, node5);
+		whatShouldArrayListContainNow.add(node5);
+		whatShouldArrayListContainNow.add(node4);
+		whatShouldArrayListContainNow.add(node3);
+		whatShouldArrayListContainNow.add(node2);
+		whatShouldArrayListContainNow.add(node1);
+		whatShouldArrayListContainNow.add(node5);
+		whatShouldArrayListContainNow.add(node4);
+		whatShouldArrayListContainNow.add(node3);
+		whatShouldArrayListContainNow.add(node2);
+		whatShouldArrayListContainNow.add(node1);
+		assertTrue(testParameters(10, 10, whatShouldArrayListContainNow));
+		arraylist.add(0, node1);
+		whatShouldArrayListContainNow.add(0, node1);
+		assertTrue(testParameters(20, 11, whatShouldArrayListContainNow));
 	}
 	@Test
 	public void testAdd7() {
@@ -124,11 +162,11 @@ public class MyArrayListTests {
 		whatShouldArrayListContainNow.add(node1);
 		whatShouldArrayListContainNow.add(node2);
 		whatShouldArrayListContainNow.add(node3);
-		assertTrue(testParameters(11, 3, whatShouldArrayListContainNow));
+		assertTrue(testParameters(10, 3, whatShouldArrayListContainNow));
 		
 		arraylist.clear();
 		whatShouldArrayListContainNow.clear();
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testClear2() {	
@@ -140,17 +178,17 @@ public class MyArrayListTests {
 		whatShouldArrayListContainNow.add(node1);
 		whatShouldArrayListContainNow.add(node2);
 		whatShouldArrayListContainNow.add(node3);
-		assertTrue(testParameters(22, 3, whatShouldArrayListContainNow));
+		assertTrue(testParameters(20, 3, whatShouldArrayListContainNow));
 		
 		arraylist.clear();
 		whatShouldArrayListContainNow.clear();
-		assertTrue(testParameters(22, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(20, 0, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testGetSize1() {
 		//Case: empty array
 		assertEquals(0, arraylist.size());
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testGetSize2() {
@@ -162,7 +200,7 @@ public class MyArrayListTests {
 		whatShouldArrayListContainNow.add(node2);
 		whatShouldArrayListContainNow.add(node3);
 		assertEquals(3, arraylist.size());
-		assertTrue(testParameters(11, 3, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 3, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testGetSize3() {
@@ -173,26 +211,26 @@ public class MyArrayListTests {
 		arraylist.remove(node1);
 		whatShouldArrayListContainNow.add(node3);
 		assertEquals(1, arraylist.size());
-		assertTrue(testParameters(11, 1, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 1, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testResize1() {
 		//Case: simple resize
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));	
 		resize();
-		assertTrue(testParameters(22, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(20, 0, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testResize2() {
 		resize();
 		resize();
-		assertTrue(testParameters(44, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(40, 0, whatShouldArrayListContainNow));	
 		resize();
-		assertTrue(testParameters(88, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(80, 0, whatShouldArrayListContainNow));	
 		resize();
-		assertTrue(testParameters(176, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(160, 0, whatShouldArrayListContainNow));	
 		resize();
-		assertTrue(testParameters(352, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(320, 0, whatShouldArrayListContainNow));	
 	}
 	
 	@Test
@@ -219,17 +257,16 @@ public class MyArrayListTests {
 		whatShouldArrayListContainNow.add(node1);
 		whatShouldArrayListContainNow.add(node1);
 		whatShouldArrayListContainNow.add(node1);
-		whatShouldArrayListContainNow.add(node1);
-		assertTrue(testParameters(11, 11, whatShouldArrayListContainNow));
+		assertTrue(testParameters(10, 10, whatShouldArrayListContainNow));
 		arraylist.add(0, node1);
 		whatShouldArrayListContainNow.add(node1);
-		assertTrue(testParameters(22, 12, whatShouldArrayListContainNow));
+		assertTrue(testParameters(20, 12, whatShouldArrayListContainNow));
 	}
 	@Test
 	public void testGet1() {
 		//Case: invalid gets
 		assertEquals(null, arraylist.get((Integer)null));
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testGet2() {
@@ -244,18 +281,16 @@ public class MyArrayListTests {
 		assertEquals(null, arraylist.get(7));
 		assertEquals(null, arraylist.get(8));
 		assertEquals(null, arraylist.get(9));
-		assertEquals(null, arraylist.get(10));
-		assertEquals(null, arraylist.get(11));
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testGet3() {
 		//Case: simple get
 		arraylist.add(0, node1);
 		whatShouldArrayListContainNow.add(node1);
-		assertTrue(testParameters(11, 1, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 1, whatShouldArrayListContainNow));	
 		assertEquals(node1, arraylist.get(0));
-		assertTrue(testParameters(11, 1, whatShouldArrayListContainNow));
+		assertTrue(testParameters(10, 1, whatShouldArrayListContainNow));
 	}
 	@Test
 	public void testGet4() {
@@ -266,12 +301,12 @@ public class MyArrayListTests {
 		whatShouldArrayListContainNow.add(0, node1);
 		whatShouldArrayListContainNow.add(3, node2);
 		whatShouldArrayListContainNow.add(6, node3);
-		assertTrue(testParameters(11, 3, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 3, whatShouldArrayListContainNow));	
 		
 		assertEquals(node1, arraylist.get(0));
 		assertEquals(node2, arraylist.get(3));
 		assertEquals(node3, arraylist.get(6));
-		assertTrue(testParameters(11, 3, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 3, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testGet5() {
@@ -282,13 +317,13 @@ public class MyArrayListTests {
 		whatShouldArrayListContainNow.add(node1.getId(), node1);
 		whatShouldArrayListContainNow.add(node2.getId(), node2);
 		whatShouldArrayListContainNow.add(node3.getId(), node3);
-		assertTrue(testParameters(11, 3, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 3, whatShouldArrayListContainNow));	
 		
 		arraylist.remove(node1.getId());
 		arraylist.remove(node2.getId());
 		arraylist.remove(node3.getId());
 		whatShouldArrayListContainNow.clear();
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));	
 
 		assertEquals(null, arraylist.get(0));
 		assertEquals(null, arraylist.get(1));
@@ -300,47 +335,45 @@ public class MyArrayListTests {
 		assertEquals(null, arraylist.get(7));
 		assertEquals(null, arraylist.get(8));
 		assertEquals(null, arraylist.get(9));
-		assertEquals(null, arraylist.get(10));
-		assertEquals(null, arraylist.get(11));
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testGet7() {
 		//Case: getting keys after over writing an add
 		arraylist.add(node1.getId(), node1);
 		whatShouldArrayListContainNow.add(node1.getId(), node1);
-		assertTrue(testParameters(11, 1, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 1, whatShouldArrayListContainNow));	
 		assertEquals(node1, arraylist.get(node1.getId()));
 
 		arraylist.add(node1.getId(), node2);
 		whatShouldArrayListContainNow.clear();
 		whatShouldArrayListContainNow.add(node1.getId(), node2);
-		assertTrue(testParameters(11, 1, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 1, whatShouldArrayListContainNow));	
 		assertEquals(node2, arraylist.get(node1.getId()));
 	}
 	@Test
 	public void testRemove1() {
 		//Case: invalid remove
 		assertEquals(null, arraylist.remove(null));
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testRemove2() {
 		//Case: removing non existent keys
 		assertEquals(null, arraylist.remove("1"));
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testRemove3() {
 		//Case: removing at invalid indexes
 		assertEquals(null, arraylist.remove(1000));
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testRemove4() {
 		//Case: removing at empty indexes
 		assertEquals(null, arraylist.remove(2));
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));	
 	}
 	@Test
 	public void testRemove5() {
@@ -351,13 +384,13 @@ public class MyArrayListTests {
 		whatShouldArrayListContainNow.add(node2.getId(), node2);
 		whatShouldArrayListContainNow.add(node3.getId(), node3);
 		whatShouldArrayListContainNow.add(node4.getId(), node4);
-		assertTrue(testParameters(11, 3, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 3, whatShouldArrayListContainNow));	
 		
 		Node x = (Node) arraylist.remove(node2.getId());
 		Node y = (Node) arraylist.remove(node3.getId());
 		Node z = (Node) arraylist.remove(node4.getId());
 		whatShouldArrayListContainNow.clear();
-		assertTrue(testParameters(11, 0, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 0, whatShouldArrayListContainNow));	
 		assertEquals(node2, x);
 		assertEquals(node3, y);
 		assertEquals(node4, z);
@@ -378,13 +411,13 @@ public class MyArrayListTests {
 		whatShouldArrayListContainNow.add(node1.getId(), node1);
 		whatShouldArrayListContainNow.add(node2.getId(), node2);
 		whatShouldArrayListContainNow.add(node3.getId(), node3);
-		assertTrue(testParameters(11, 3, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 3, whatShouldArrayListContainNow));	
 		
 		Node x = (Node) arraylist.remove(node1.getId());
 		Node y = (Node) arraylist.remove(node3.getId());
 		whatShouldArrayListContainNow.remove(node1);
 		whatShouldArrayListContainNow.remove(node3);		
-		assertTrue(testParameters(11, 1, whatShouldArrayListContainNow));	
+		assertTrue(testParameters(10, 1, whatShouldArrayListContainNow));	
 		assertEquals(node1, x);
 		assertEquals(node3, y);
 		
@@ -396,7 +429,7 @@ public class MyArrayListTests {
 		assertEquals("", null, z);
 		
 		x = (Node) arraylist.remove(node1.getId());
-		assertTrue(testParameters(11, 1, whatShouldArrayListContainNow));
+		assertTrue(testParameters(10, 1, whatShouldArrayListContainNow));
 		assertEquals("", null, x);
 	}
 	@Test
@@ -408,7 +441,7 @@ public class MyArrayListTests {
 		whatShouldArrayListContainNow.add(node2.getId(), node2);
 		whatShouldArrayListContainNow.add(node3.getId(), node3);
 		whatShouldArrayListContainNow.add(node4.getId(), node4);
-		assertTrue(testParameters(11, 3, whatShouldArrayListContainNow));
+		assertTrue(testParameters(10, 3, whatShouldArrayListContainNow));
 	
 		assertEquals(false, arraylist.contains(node1));
 		assertEquals(true,  arraylist.contains(node2));
@@ -419,7 +452,7 @@ public class MyArrayListTests {
 		arraylist.remove(node3.getId());
 		whatShouldArrayListContainNow.remove(node2);
 		whatShouldArrayListContainNow.remove(node3);		
-		assertTrue(testParameters(11, 1, whatShouldArrayListContainNow));
+		assertTrue(testParameters(10, 1, whatShouldArrayListContainNow));
 
 		assertEquals("", false, arraylist.contains(node1));
 		assertEquals("", false, arraylist.contains(node2));
@@ -481,31 +514,36 @@ public class MyArrayListTests {
 		assertEquals(testSize, testNumberOfEntries());
 		
 		//Testing Content
-		//ArrayList temp = testContent();
-		assertEquals(content, arraylist);
+//		ArrayList temp = testContent();
+//		assertEquals(content, temp);
+		assertArrayEquals(content.toArray(), arraylist.toArray());
 		
 		//Testing Load Factor
 		assertTrue(testCapacity());
 		return true;		
 	}
 	public boolean testCapacity() {
-		return (arraylist.size() < getCapacity());
+		return (arraylist.size() <= getCapacity());
 	}
 	public int testNumberOfEntries() {
-		int i = 0;
-		for(Object o : getArray()) {
-			if(o!=null) {
-				i++;
-			}
-		}
-		return i;
+//		int i = 0;
+//		System.out.println("a:"+Arrays.toString(arraylist.toArray()));
+//		for(Object o : arraylist.toArray()) {
+//			System.out.println(o);
+//			if(o!=null) {
+//				i++;
+//			}
+//		}
+//		System.out.println("i"+ i);
+//		return i;
+		return arraylist.toArray().length;
 	}
 //	public ArrayList testContent() {
-//		ArrayList content = new ArrayList();
+//		ArrayList temp = new ArrayList();
 //		for(int i = 0; i<getArray().length; i++) {
-//			content.add(getArray()[i]);
+//			temp.add(getArray()[i]);
 //		}
-//		return content;
+//		return temp;
 //	}
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
